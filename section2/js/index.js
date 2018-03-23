91028112f1b5bac87f2ec2a4199c99d97545e524
DESKTOP-0D0D8FB\Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-03-22 22:42:13 -0300

    section2 finished

---
+++ section2/js/index.js
@@ -0,0 +1,38 @@
+// Javascript Data types
+// =====================
+
+/**
+* Boolean
+* Null
+* Undefined
+* Number
+* String
+* Symbol
+*/
+
+//NaN
+// var num = 'iuhiusadhas';
+// alert(Number(num));
+//
+// num = '100';
+// alert(Number(num));
+//
+// //isNaN
+// alert(isNaN(NaN)); //true
+// alert(isNaN(100)); //false
+// alert(isNaN('blue')); //true
+// alert(isNaN(true)); // true becomes 1 - false becomes 0
+//
+// var num1 = parseInt("123ikjkljashf");
+// alert(num1);
+// var num2 = parseInt("20.958");
+// alert(num2);
+// var num3 = parseFloat("20.958");
+// alert(num3);
+
+var name = 'Ricardo\n Oliveira'; // \t (tab) \b(backspace)
+console.log(name.length);
+
+var age = 30;
+var ageStringfy = age.toString();
+console.log(typeof ageStringfy);
