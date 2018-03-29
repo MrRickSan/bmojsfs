03aa7a7c354c65e34c86b8aa35c5c7e0ede298cf
DESKTOP-0D0D8FB\Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-03-29 00:06:48 -0300

    section10

---
+++ section10/js/index.js
@@ -0,0 +1,45 @@
+/**
+* JavaScript Functions
+* =====================
+* function declaration are available in the execution context
+* function expression are not complete until the execution reaches
+* that line of code
+*/
+
+// function declaration
+console.log(total(1, 2))
+
+function total (num1, num2) {
+  return num1 + num2
+}
+
+// function expression
+var totalExp = function (num1, num2) {
+  return num1 + num2
+}
+
+console.log(totalExp(1, 2))
+
+// function that receives and calls another function
+function callingAFunction (myFunc, myArgs) {
+  return myFunc(myArgs)
+}
+
+function addHundred (num) {
+  return num + 100
+}
+
+var result = callingAFunction(addHundred, 1000)
+console.log(result)
+
+// function object also has additional property 'caller' which contains
+// a reference to the function that called this function or null
+function factorial (num) {
+  if (num < 1) {
+    return 1
+  } else {
+    return num * factorial(num - 1)
+    // return num * arguments.callee(num - 1) // use deprecated
+  }
+}
+console.log(factorial(5))
